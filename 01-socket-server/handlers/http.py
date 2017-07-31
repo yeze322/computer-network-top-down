f45a5cfa534dbe7fe1fba376e35180e673e1d652
Ze Ye <zeye@microsoft.com>
2017-07-31 21:50:43 +0800

    simple file server & assets

--- 01-socket-server/handlers/http.py
+++ 01-socket-server/handlers/http.py
@@ -1,12 +1,54 @@
+import os
 outputdata = 'helloworld'
+dir = os.path.join(os.path.dirname(__file__), '../assets')
+
+def guessFileType(fileName):
+  if fileName.endswith('ico'):
+    return 'image/x-icon'
+  elif fileName.endswith('html'):
+    return 'text/html'
+  elif fileName.endswith('json'):
+    return 'text/json'
+  elif fileName.endswith('css'):
+    return 'text/css'
+  elif fileName.endswith('js'):
+    return 'application/javascript'
+  else:
+    return None
 
 def HTTPHandler(conn):
   message = conn.recv(1024)
   print('message: ', message)
+
   if not message:
     return
-  outputdata = open('index.html').read()
-  header = 'HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html\nContent-Length: %d\n\n' % (len(outputdata))
+
+  lines = message.split()
+  
+  method = lines[0].decode()
+  if method != 'GET':
+    header = 'HTTP/1.1 404 Not Found'
+    conn.send(header.encode())
+    return
+
+  route = lines[1].decode()
+  if route == '/':
+    route = '/index.html'
+
+  outputdata = None
+  filePath = dir + route
+  try:
+    print('loading file: ', filePath)
+    outputdata = open(filePath, 'rb').read()
+  except IOError:
+    print('File not exist! ', route)
+    header = 'HTTP/1.1 404 Not Found'
+    conn.send(header.encode())
+    return
+
+  fileType = guessFileType(route) or 'text/plain'
+  header = 'HTTP/1.1 200 OK\nConnection: close\nContent-Type: %s\nContent-Length: %d\n\n' % (fileType, len(outputdata))
+
   conn.send(header.encode())
-  conn.send(outputdata.encode())
+  conn.send(outputdata)
   return
